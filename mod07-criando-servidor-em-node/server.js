635ccf8d3b0696cc2af8b31c0f487b8184e5ad59
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-05-15 21:32:25 -0300

    listando e deletando livros

--- mod07-criando-servidor-em-node/server.js
+++ mod07-criando-servidor-em-node/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const nedb = require('nedb');
-const bodyParser = require('body-parser')
-const db = new nedb({filename: './database.db', autoload: true}) ;
+const bodyParser = require('body-parser');
+const db = new nedb({filename: './database.db', autoload: true});
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/books', (req, res) => {
-    res.json({
-        success: true
-    })
-})
+    db.find({}, (err, books) => {
+        res.json(books);
+    });
+});
 
 app.post('/books', (req, res) => {
     db.insert({
@@ -19,9 +19,17 @@ app.post('/books', (req, res) => {
         author: req.body.author
     }, (err, doc) => {
         res.json(doc);
-    })
+    });
+});
+
+app.delete('/books/:id', (req, res) => {
+    db.remove({
+        _id: req.params.id
+    }, (err) => {
+        res.json({success:true});
+    });
 });
 
 app.listen(3000, () => {
-    console.log('servidor executando com sucesso na porta 3000')
+    console.log('servidor executando com sucesso na porta 3000');
 });
