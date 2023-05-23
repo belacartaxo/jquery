abeffad5826c294bd66a51d7b33a28ec2912ba7c
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-05-23 17:11:34 -0300

    att

---
+++ mod10-plugin-de-loading/loader.js
@@ -0,0 +1,54 @@
+(function($){
+    $.fn.load = function(action = 'load'){
+        return this.each(function(){
+
+            switch(action){
+                case 'load':
+
+                    let divToAppend = document.createElement('div');
+                    divToAppend.id = 'loader';
+        
+                    $(divToAppend).css({
+                        'width': '100%',
+                        'height': '100%', 
+                        'position': 'fixed', 
+                        'top': 0, 
+                        'left': 0, 
+                        'text-align': 'center', 
+                        'background-color': 'rgba(0, 0, 0, 0.4)'
+                    });
+        
+                    let divLoad = document.createElement('div');
+                    divLoad.className = 'lds-dual-ring';
+        
+                    $(divLoad).css({
+                        'position': 'absolute',
+                        'top': '50%', 
+                        'left': '50%', 
+                        'transform': 'translate(-50%, -50%)'
+                    });
+        
+                    $(divToAppend).append(divLoad);
+        
+                    $(this).append(divToAppend);
+
+                break;
+
+                case 'unload':
+                    
+                $('#loader').fadeOut(function(){
+                    $(this).remove();
+                })
+                
+                break;
+
+                default:
+                    console.log('Informe uma ação válida');
+                break;
+            }
+            
+            
+        });
+    }
+
+})(jQuery);
