75e9c3c8a8b4fdcd788ffb350297b6921e7dfd22
git/Isabela Cartaxo <belacartaxo@gmail.com>
2023-07-29 13:37:35 -0300

    att mod 12

---
+++ mod12-projeto-elevador/assets/js/Elevator.js
@@ -0,0 +1,25 @@
+class Elevator{
+    constructor() {
+        this.$elevator = $('.elevator');
+    }
+
+    openDoor(){
+        if(this.isDoorOpen()){
+            return true
+        } else {
+            this.$elevator.find('.door').addClass('open');
+        }
+    }
+
+    closeDoor(){
+        if(this.isDoorOpen()){
+            this.$elevator.find('.door').removeClass('open');
+        } else {
+            return true
+        }
+    }
+
+    isDoorOpen(){  
+        return(doors.hasClass('open'));
+    }
+}
